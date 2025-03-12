877a75578154f20670001e0c7d74c7893cf6d4c1
Chamath Harshana <99314096+IT21175466@users.noreply.github.com>
2025-03-12 19:09:40 +0530

    feat(puzzle):Host URL Updated

--- frontend/lib/constants/constant.dart
+++ frontend/lib/constants/constant.dart
@@ -2,4 +2,4 @@ const MONGO_URL =
     "mongodb+srv://chamath:t5WO6Xex83mYQokm@cluster0.onpja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const COLLECTION_NAME = "Puzzle";
 
-const ML_API = "http://10.237.93.205:5000";
+const ML_API = "http://54.169.162.129:5000";
