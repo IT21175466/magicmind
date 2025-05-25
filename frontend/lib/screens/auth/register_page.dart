639da02b5cf922b74c29257c87bdfc089d153fc1
Chamath Harshana <99314096+IT21175466@users.noreply.github.com>
2025-05-25 20:19:51 +0530

    UI Fix

--- frontend/lib/screens/auth/register_page.dart
+++ frontend/lib/screens/auth/register_page.dart
@@ -55,91 +55,94 @@ class _RegisterPageState extends State<RegisterPage> {
           ),
         ),
         padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("🌈 Let’s Create Your Account!",
-                style: GoogleFonts.poppins(
-                    fontSize: 26, fontWeight: FontWeight.bold)),
-            SizedBox(height: 80),
-            TextField(
-              controller: nameCtrl,
-              decoration: InputDecoration(
-                labelText: 'Your Name',
-                prefixIcon: Icon(Icons.person),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100,
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              controller: emailCtrl,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              Text("🌈 Let’s Create Your Account!",
+                  style: GoogleFonts.poppins(
+                      fontSize: 26, fontWeight: FontWeight.bold)),
+              SizedBox(height: 80),
+              TextField(
+                controller: nameCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Your Name',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              controller: passwordCtrl,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock_outline),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              SizedBox(height: 15),
+              TextField(
+                controller: emailCtrl,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            isLoading
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      if (emailCtrl.text.isNotEmpty &&
-                          passwordCtrl.text.isNotEmpty) {
-                        await register(context);
-                      } else {
+              SizedBox(height: 15),
+              TextField(
+                controller: passwordCtrl,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock_outline),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ),
+              SizedBox(height: 30),
+              isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : ElevatedButton(
+                      onPressed: () async {
                         setState(() {
-                          isLoading = false;
+                          isLoading = true;
                         });
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text("Please fill all fields")));
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                    ),
-                    child: Text(
-                      "Done!",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Andika',
-                        color: Colors.white,
+                        if (emailCtrl.text.isNotEmpty &&
+                            passwordCtrl.text.isNotEmpty) {
+                          await register(context);
+                        } else {
+                          setState(() {
+                            isLoading = false;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Please fill all fields")));
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                      child: Text(
+                        "Done!",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Andika',
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-            TextButton(
-              onPressed: () => backToLogin(context),
-              child: Text("👋 Already have an account? Log in",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontFamily: 'Andika',
-                    fontSize: 16,
-                  )),
-            ),
-          ],
+              TextButton(
+                onPressed: () => backToLogin(context),
+                child: Text("👋 Already have an account? Log in",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontFamily: 'Andika',
+                      fontSize: 16,
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
