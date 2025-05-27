cbc71904f8b1775a249225c468bec0a85f08240a
PRAVEEN_MADANAYAKE <sidath.madanayake99@gmail.com>
2025-05-27 21:13:55 +0530

    add changes

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:magicmind_puzzle/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
